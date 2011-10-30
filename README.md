5d21981c82af7328f469f438bf66f964444f6330
Mike Stipicevic <stipredirect@gmail.com>
2011-10-29 17:24:32 -0700

    added libevent handling

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-smidgen is a small daemon that waits on keyboard events to disable the trackpad while typing
+smidgen is a small daemon that waits on keyboard events to disable the trackpad
+while typing
 
-based on syndaemon and fspd: ( http://int.ed.ntnu.no/fspd.tar or https://int.ed.ntnu.no/svn/public/fspd)
+based on syndaemon and fspd: ( http://int.ed.ntnu.no/fspd.tar or
+https://int.ed.ntnu.no/svn/public/fspd)
 
-this is meant to be a stopgap until the drivers work properly, but any patches are welcome.
+this is meant to be a stopgap until the drivers work properly, but any patches
+are welcome.
+
+until we have a full build, run `gcc -O2 -o smidgen smidgen.c` to build; and
+`sudo ./smidgen <kb event file>` to run. On my system, that file is
+`/dev/input/by-path/platform-i8042-serio-0-event-kbd`
+
--- src/smidgen.c
+++ src/smidgen.c
@@ -1,7 +1,16 @@
 // an initial cut at a select-based inotify reader
+// compile me with gcc -O2 -o smidgen smidgen.c -levent
+// you will need libevent-dev installed on your system (for debian)
 
-// thanks to http://qaa.ath.cx/inotify.html and http://developerweb.net/viewtopic.php?id=2933
-// for the example code
+/*
+ * references:
+ * http://qaa.ath.cx/inotify.html
+ * http://developerweb.net/viewtopic.php?id=2933
+ * tupleserver
+ * cliserver
+ * https://github.com/wereHamster/transmission/blob/master/third-party/libevent/test/test-time.c
+ *
+ */
 
 #include <signal.h>
 #include <stdio.h>
@@ -9,8 +18,10 @@
 #include <sys/select.h>
 #include <unistd.h>
 #include <errno.h>
+#include <event.h>
+#include <fcntl.h>
 
-#define ARGS IN_ONESHOT | IN_ALL_EVENTS
+#define ARGS IN_ONESHOT | IN_ACCESS
 
 #define EVENT_SIZE 1024
 
@@ -22,9 +33,83 @@ int g_fd;
 
 uint32_t g_wd;
 
+static struct event_base *server_loop;
+
 void control_c(int ignored) {
 	inotify_rm_watch(g_fd, g_wd);
+  fprintf(stderr, "attempting to quit...");
+	if(event_base_loopexit(server_loop, NULL)) {
+		fprintf(stderr,"Error shutting down server\n");
+	}
 	_exit (0);
+}
+
+static const char *fn;
+static struct event ev;
+
+void control_mouse(int enable){
+  const char *flag = enable ? "1" : "0";
+
+  pid_t pID = vfork();
+  if(0 == pID){
+    int ret = execl("/usr/bin/xinput", "/usr/bin/xinput", "set-int-prop", "12", "Device Enabled", "8", flag, (char *) 0);
+    if (-1 == ret){
+      perror("error executing xinit!");
+    }
+  } else if(pID < 0){
+    perror("error forking to xinit!!!");
+  }
+}
+
+////////////
+static void time_cb(int fd, short event, void *arg)
+{
+  control_mouse(1);
+
+  //evtimer_del(&ev);
+}
+
+static void handle_event(int g_fd, short evtype, void *arg){
+  int len = read (g_fd, buf, BUF_LEN);
+
+
+  evtimer_del(&ev);
+
+  if (len > 0) {
+    control_mouse(0);
+
+    struct timeval tv;
+    tv.tv_sec = 0;
+    tv.tv_usec = 200000;
+    evtimer_add(&ev, &tv);
+  }
+
+  g_wd = inotify_add_watch(g_fd,
+      fn,
+      ARGS);
+
+  if (g_wd < 0) {
+    perror ("inotify_add_watch");
+  }
+  // fflush(0);
+}
+
+static int set_nonblock(int fd)
+{
+	int flags;
+
+	flags = fcntl(fd, F_GETFL);
+	if(flags == -1) {
+		fprintf(stderr,"Error getting flags on fd %d", fd);
+		return -1;
+	}
+	flags |= O_NONBLOCK;
+	if(fcntl(fd, F_SETFL, flags)) {
+		fprintf(stderr,"Error setting non-blocking I/O on fd %d", fd);
+		return -1;
+	}
+
+	return 0;
 }
 
 int main(int argc, char*argv[]){
@@ -33,6 +118,8 @@ int main(int argc, char*argv[]){
     return 1;
   }
 
+  fn = argv[1];
+
 	signal(SIGINT, control_c);
 
 	g_fd = inotify_init();
@@ -40,39 +127,49 @@ int main(int argc, char*argv[]){
 	        perror ("inotify_init");
 	}
 	g_wd = inotify_add_watch(g_fd,
-		argv[1],
+		fn,
 		ARGS);
 
 	if (g_wd < 0) {
     perror ("inotify_add_watch");
   }
 
-  while(1){
-    int result;
-    fd_set readset;
-    do {
-      FD_ZERO(&readset);
-      FD_SET(g_fd, &readset);
-      result = select(g_fd + 1, &readset, NULL, NULL, NULL);
-    } while (result == -1 && errno == EINTR);
+	// Initialize libevent
+	printf("libevent version: %s\n", event_get_version());
+	struct event_base *evloop = event_base_new();
+	if(NULL == evloop) {
+		perror("Error initializing event loop.\n");
+		return -1;
+	}
+	server_loop = evloop;
+	printf("libevent is using %s for events.\n", event_base_get_method(evloop));
+	// Set socket for non-blocking I/O
+	if(set_nonblock(g_fd)) {
+		perror("Error setting listening socket to non-blocking I/O.\n");
+		return -1;
+	}
 
-    if(result > 0){
-      int len = read (g_fd, buf, BUF_LEN);
+	struct event connect_event;
+	// Add an event to wait for connections
+	event_set(&connect_event, g_fd, EV_READ | EV_PERSIST, handle_event, evloop);
+	event_base_set(evloop, &connect_event);
+	if(event_add(&connect_event, NULL)) {
+		fprintf(stderr,"Error scheduling connection event on the event loop.\n");
+	}
 
-      if (len > 0) {
-        printf("events seen: %d\n",len/sizeof(struct inotify_event));
-      }
+  evtimer_set(&ev, time_cb, NULL);
+	event_base_set(evloop, &ev);
 
-      g_wd = inotify_add_watch(g_fd,
-          argv[1],
-          ARGS);
+	// Start the event loop
+	if(event_base_dispatch(evloop)) {
+		fprintf(stderr,"Error running event loop.\n");
+	}
 
-      if (g_wd < 0) {
-        perror ("inotify_add_watch");
-      }
-      // fflush(0);
-    }
-  }
+	// Clean up libevent
+	if(event_del(&connect_event)) {
+		fprintf(stderr,"Error removing connection event from the event loop.\n");
+	}
+	event_base_free(evloop);
 
   return 0;
 }
